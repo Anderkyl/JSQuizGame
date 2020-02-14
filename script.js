298dba20b596d74d433c48b24abfeb19bb3ef795
anderkyl001 <anderkyl001@warren.k12.in.us>
2020-02-14 10:57:05 -0500

    Got the question

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 
-let createQuestion = function("What color is the sky?", "Blue", "White", "purple","Color is an illusion", "color is an illusion"){
+let doTheGame = function(){
+
+    createQuestion("What color is the sky?", "Blue", "White", "Purple", "Color is an illusion");
+    return createQuestion;
+
 
 }
+
+
+
+
+
+
+
+let createQuestion = function(questionText, choice1, choice2, choice3, choice4, correctChoice){
+
+
+
+    let btn0 = document.getElementById("choice1");
+    btn0.innerText = choice1;
+
+    let btn1 = document.getElementById("btn1");
+    btn1.innerText = choice2;
+
+    let btn2 = document.getElementById("btn2");
+    btn2.innerText = choice3;
+
+    let btn3 = document.getElementById("btn3");
+    btn3.innerText = choice4;
+
+
+
+
+};
