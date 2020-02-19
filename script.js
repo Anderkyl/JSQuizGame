145656b8c14025a1e69c401b637f05167b622bfb
anderkyl001 <anderkyl001@warren.k12.in.us>
2020-02-19 10:59:00 -0500

    Got the question

--- script.js
+++ script.js
@@ -2,34 +2,26 @@
 let doTheGame = function(){
 
     createQuestion("What color is the sky?", "Blue", "White", "Purple", "Color is an illusion");
-    return createQuestion;
+    createQuestion("How much wood does a woodchuck Chuck?", "No wood", "What's a woodchuck?", "As much as a woodchuck could chuck wood","Yeah, what he said.");
 
 
-}
+};
 
 
+let createQuestion = function(questionText, choice1, choice2, choice3, choice4, correctChoice) {
 
+    let placeQuestion = document.getElementById("question");
+    placeQuestion.innerText = questionText;
 
-
-
-
-let createQuestion = function(questionText, choice1, choice2, choice3, choice4, correctChoice){
-
-
-
-    let btn0 = document.getElementById("choice1");
+    let btn0 = document.getElementById("choice0");
     btn0.innerText = choice1;
 
-    let btn1 = document.getElementById("btn1");
+    let btn1 = document.getElementById("choice1");
     btn1.innerText = choice2;
 
-    let btn2 = document.getElementById("btn2");
+    let btn2 = document.getElementById("choice2");
     btn2.innerText = choice3;
 
-    let btn3 = document.getElementById("btn3");
+    let btn3 = document.getElementById("choice3");
     btn3.innerText = choice4;
-
-
-
-
 };
