90cf2b8cf4197721def3caf0309a6984df244818
anderkyl001 <anderkyl001@warren.k12.in.us>
2020-02-20 10:58:49 -0500

    Got the question

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 
 let doTheGame = function(){
 
-    createQuestion("What color is the sky?", "Blue", "White", "Purple", "Color is an illusion");
-    createQuestion("How much wood does a woodchuck Chuck?", "No wood", "What's a woodchuck?", "As much as a woodchuck could chuck wood","Yeah, what he said.");
+    createQuestion("What color is the sky?", "Color is an illusion", "White", "Purple", "Blue", "Blue");
+    btn0.onclick = createQuestion2;
+    //createQuestion2("How much wood does a woodchuck Chuck?", "No wood", "What's a woodchuck?", "As much as a woodchuck could chuck wood","Yeah, what he said.","As Much as a woodchuck could chuck wood.");
 
 
 };
 
 
 let createQuestion = function(questionText, choice1, choice2, choice3, choice4, correctChoice) {
+
+    placeQuestion = document.getElementById("question");
+    placeQuestion.innerText = questionText;
+
+    btn0 = document.getElementById("choice0");
+    btn0.innerText = choice1;
+   // btn0.onclick = function(){createQuestion(this)};
+
+    btn1 = document.getElementById("choice1");
+    btn1.innerText = choice2;
+
+    btn2 = document.getElementById("choice2");
+    btn2.innerText = choice3;
+
+    btn3 = document.getElementById("choice3");
+    btn3.innerText = choice4;
+
+};
+let createQuestion2 = function(questionText, choice1, choice2, choice3, choice4, correctChoice) {
+    createQuestion2("How much wood does a woodchuck Chuck?", "No wood", "What's a woodchuck?", "As much as a woodchuck could chuck wood","Yeah, what he said.","As Much as a woodchuck could chuck wood.");
 
     let placeQuestion = document.getElementById("question");
     placeQuestion.innerText = questionText;
@@ -24,4 +45,11 @@ let createQuestion = function(questionText, choice1, choice2, choice3, choice4, 
 
     let btn3 = document.getElementById("choice3");
     btn3.innerText = choice4;
+
 };
+
+let checkAnswer = function(correctChoice){
+    if(correctChoice.id === "correct"){
+        createQuestion()
+    }
+}
