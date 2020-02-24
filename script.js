9b5ac817d280bdb1e576db8d617b8486a770b7cf
anderkyl001 <anderkyl001@warren.k12.in.us>
2020-02-24 10:53:29 -0500

    aHaHa

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let count = 0;
-// let numRight = 0;
-// let correctOne;
-// let numQuestions
-
+let numRight = 0;
+let correctOne;
+let numQuestions;
+correctChoice = "correctBox";
 
 
 let doTheGame = function(){
+    btn1.onclick = function(){increaseCount(this)};
+    btn2.onclick = function(){increaseCount(this)};
+    btn3.onclick = function(){increaseCount(this)};
+
 
     if(count===0){
 
         createQuestion("What color is the sky?", "Color is an illusion", "White", "Purple", "Blue", "Blue");
     }else if(count===1){
         createQuestion("What is 'one' in japanese?","Ichi","Ni","San","Yon","Ichi");
-    }
+    }else if(count===3){
+        createQuestion("Who drives the struggle bus?","Me","you","Mr. Law","Most definitely me.","Most definitely me.");
+    }else {
+            let question = document.getElementById("question");
+            question.innerText = "You got" + numRight + " correct out of " + numQuestions;
+            document.getElementById("buttons").innerHTML = " ";
+        }
+
+    document.getElementById("progress").innerText = "Question " + (count + 1) + " of " + numQuestions;
 
 };
 
@@ -26,8 +38,6 @@ let createQuestion = function(questionText, choice1, choice2, choice3, choice4, 
     option1.innerText = choice1;
 
     button = document.getElementById("btn0");
-    button.onclick = increaseCount();
-
 
     btn1 = document.getElementById("choice1");
     btn1.innerText = choice2;
@@ -41,9 +51,6 @@ let createQuestion = function(questionText, choice1, choice2, choice3, choice4, 
 };
 
 let increaseCount = function(){
-
-        count++;
-        doTheGame();
-
-
+    count++;
+    doTheGame();
 };
