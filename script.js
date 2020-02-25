57df3aefe0c8801c7a33a913bbe1ef9e74914396
anderkyl001 <anderkyl001@warren.k12.in.us>
2020-02-25 10:59:14 -0500

    aHaHa

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let count = 0;
 let numRight = 0;
-let correctOne;
 let numQuestions;
-correctChoice = "correctBox";
+let correct;
 
 
 let doTheGame = function(){
-    btn1.onclick = function(){increaseCount(this)};
-    btn2.onclick = function(){increaseCount(this)};
-    btn3.onclick = function(){increaseCount(this)};
+    btn0.onclick = function(){checkCorrect(this)};
+    btn1.onclick = function(){checkCorrect(this)};
+    btn2.onclick = function(){checkCorrect(this)};
+    btn3.onclick = function(){checkCorrect(this)};
 
 
     if(count===0){
@@ -16,15 +16,16 @@ let doTheGame = function(){
         createQuestion("What color is the sky?", "Color is an illusion", "White", "Purple", "Blue", "Blue");
     }else if(count===1){
         createQuestion("What is 'one' in japanese?","Ichi","Ni","San","Yon","Ichi");
-    }else if(count===3){
+    }else if(count===2){
         createQuestion("Who drives the struggle bus?","Me","you","Mr. Law","Most definitely me.","Most definitely me.");
     }else {
             let question = document.getElementById("question");
             question.innerText = "You got" + numRight + " correct out of " + numQuestions;
-            document.getElementById("buttons").innerHTML = " ";
+            document.getElementById("BUTTON").innerHTML = " ";
         }
 
     document.getElementById("progress").innerText = "Question " + (count + 1) + " of " + numQuestions;
+
 
 };
 
@@ -39,6 +40,9 @@ let createQuestion = function(questionText, choice1, choice2, choice3, choice4, 
 
     button = document.getElementById("btn0");
 
+    btn0 = document.getElementById("choice0");
+    btn0.innerText = choice1;
+
     btn1 = document.getElementById("choice1");
     btn1.innerText = choice2;
 
@@ -48,9 +52,17 @@ let createQuestion = function(questionText, choice1, choice2, choice3, choice4, 
     btn3 = document.getElementById("choice3");
     btn3.innerText = choice4;
 
+    correct=correctChoice;
+
 };
 
-let increaseCount = function(){
-    count++;
+let checkCorrect = function(button){
+    if(button.innerText===correct){
+        numRight++
+        count++;
+    }else {
+        count++;
+
+    }
     doTheGame();
 };
